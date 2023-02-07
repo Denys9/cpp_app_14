1c170ec883e3081da1fa546a9e8bd2d07b648a37
lozin <lozindenis6@gmail.com>
2023-02-07 20:41:45 +0200

    №2

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    int* x_ptr = new int(0);
+    cout << "Enter x->";
+    cin >> *x_ptr;
+    if (x_ptr < 0)
+    {
+        cout << "x_ptr negative" << endl;
+    }
+    if (x_ptr > 0) 
+    {
+        cout << "x_ptr positive" << endl;
+    }
+    if (x_ptr == 0)
+    {
+        cout << "x_ptr zero" << endl;
+    }
+
+}
