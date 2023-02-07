51b3b511775978e6a327dee1ab1bab3591c693a5
lozin <lozindenis6@gmail.com>
2023-02-07 20:33:15 +0200

    №1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
-#include <iostream>
-#include <ctime>
+#include<iostream> 
 using namespace std;
-
 
 int main()
 {
-
+    int* x_ptr = new int(0);
+    int* y_ptr = new int(0);
+    cout << "Enter x->";
+    cin >> *x_ptr;
+    cout << "Enter y->";
+    cin >> *y_ptr;
+    if (x_ptr < y_ptr) 
+    {
+        cout << *y_ptr << endl;
+    }
+    if (x_ptr > y_ptr)
+    {
+        cout << *x_ptr << endl;
+    }
+       
+    
+    delete x_ptr;
 }
